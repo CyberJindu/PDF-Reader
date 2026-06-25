f46586e918860f7146242690ae27af7cb896189a
Chris Jindu <chrisjindu04@gmail.com>
2026-06-25 07:16:04 -0600

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -12,14 +12,11 @@ router.get('/users', adminController.getAllUsers);
 // Get user details
 router.get('/users/:userId', adminController.getUserDetails);
 
-// Update user privileges (toggle ON/OFF all features)
+// Update user privileges (toggle ON/OFF premium access)
 router.put('/users/:userId/privileges', adminController.updateUserPrivileges);
 
 // Get user stats
 router.get('/users/:userId/stats', adminController.getUserStats);
-
-// Toggle specific feature
-router.patch('/users/:userId/features', adminController.toggleUserFeature);
 
 // Deactivate user
 router.delete('/users/:userId/deactivate', adminController.deactivateUser);
